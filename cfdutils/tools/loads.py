9e5f268762b9f3353817471b6e1b0b329510ee6a
acrovato <39187559+acrovato@users.noreply.github.com>
2020-09-11 16:55:11 +0200

    Basic config

---
+++ cfdutils/tools/loads.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Aerodynamic loads
+# Adrien Crovato
+# TODO check the integration direction to avoid sign reversal
+
+class Loads:
+    def __init__(self):
+        self.ys = [] # spanwise stations
+        self.chds = [] # chord
+        self.data = {} # coordinates and pressure coefficient
+        self.cls = [] # lift
+        self.cms = [] # moment positive nose-up (clockwise)
+        self.cds = [] # drag
+    
+    def add(self, y, pts, cp):
+        '''Add data for a section along the span
+        '''
+        import numpy as np
+        self.ys.append(y)
+        c = max(pts[:,0]) - min(pts[:,0])
+        self.chds.append(c)
+        x_c = np.zeros(((pts.shape[0], 1)))
+        x_c[:,0] = (pts[:,0] - min(pts[:,0])) / c
+        self.data[len(self.ys)-1] = np.hstack((pts, x_c, cp))
+
+    def compute(self, alpha = 0):
+        '''Compute the sectional aerodynamic load coefficients
+        '''
+        import numpy as np
+        for j in range(0, len(self.ys)):
+            x = self.data[j][:,0]
+            z = self.data[j][:,2]
+            cp = self.data[j][:,4]
+            c = self.chds[j] # chord
+            c_4 = np.min(x) + 0.25*c # quarter-chord position
+            # integrate pressure coefficient
+            i = 0
+            cz = 0
+            cx = 0
+            cm = 0 
+            while i < (x.shape[0]-1):
+                dx = -(x[i+1] - x[i]) / c
+                dz = -(z[i+1] - z[i]) / c
+                cz -= 0.5 * dx * (cp[i+1] + cp[i])
+                cx -= 0.5 * dz * (cp[i+1] + cp[i])
+                cm += 0.5*(cp[i+1]*(x[i+1]-c_4) + cp[i]*(x[i]-c_4)) * dx/c + 0.5*(cp[i+1]*z[i+1] + cp[i]*z[i]) * dz/c
+                i = i+1
+            # rotate to flow direction
+            cl = cz*np.cos(alpha) - cx*np.sin(alpha)
+            cd = cz*np.sin(alpha) + cx*np.cos(alpha)
+            self.cls.append(cl)
+            self.cms.append(cm)
+            self.cds.append(cd)
+        
+    def display(self):
+        '''Display the results
+        '''
+        print('y = ', self.ys)
+        print('Cl = ', self.cls)
+        print('Cm = ', self.cms)
+        print('Cd = ', self.cds)
+    
+    def write(self):
+        '''Write to disk
+        '''
+        import numpy as np
+        # pressure
+        for j in range(0, len(self.ys)):
+            print('writing pressure data file in workspace directory: slice_' + str(j) + '.dat...')
+            np.savetxt('slice_'+str(j)+'.dat', self.data[j], fmt='%1.5e', delimiter=',', header='x, y, z, x/c, Cp @ y='+str(self.ys[j]), comments='')
+        # loads
+        loads = np.hstack((self.ys, self.cls, self.cms, self.cds))
+        print('writing loads data file in workspace directory: loads.dat...')
+        np.savetxt('loads.dat', loads, fmt='%1.5e', delimiter=',', header='y, Cl, Cm, Cd', comments='')
---
+++ run.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+# -*- coding: utf8 -*-
+# test encoding: à-é-è-ô-ï-€
+
+# Copyright 2020 Adrien Crovato
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+## @package pycfdutils
+#  Python CFD post-processing utilities
+#  Adrien Crovato
+
+def setup():
+    '''Perform basic setup
+    '''
+    import sys, os, argparse
+    # Parse cl arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('file', help='path to python script to be run')
+    args = parser.parse_args()
+    # Fix paths
+    sys.path.append(os.path.dirname(os.path.realpath(__file__))) # adds "." to the python path
+    # check input filepath
+    fpath = os.path.abspath(args.file)
+    if not os.path.isfile(fpath):
+        raise Exception('file not found: ', fpath)
+    # create workspace directory
+    wdir = os.path.join(os.getcwd(), 'workspace')
+    if not os.path.isdir(wdir):
+        print('creating', wdir)
+        os.makedirs(wdir)
+    os.chdir(wdir)
+    return fpath
+
+def printStart():
+    import time, socket
+    print('*' * 80)
+    print('* pycfdutils')
+    print('* Adrien Crovato, 2020')
+    print('* Distributed under Apache license 2.0')
+    print('*' * 80)
+    print('* time:', time.strftime('%c'))
+    print('* hostname:', socket.gethostname())
+    print('*' * 80)
+    
+def printEnd():
+    print('*' * 80)
+    print('* Done!')
+    print('*' * 80)
+
+def main():
+    # start
+    fpath = setup()
+    printStart()
+    # run
+    global __file__
+    __file__ = fpath # so that latter calls to __file__ will reference the script referenced by fpath
+    exec(open(fpath, 'r', encoding='utf8').read(), globals(), globals())
+    # end
+    printEnd()
+
+if __name__ == "__main__":
+    main()
